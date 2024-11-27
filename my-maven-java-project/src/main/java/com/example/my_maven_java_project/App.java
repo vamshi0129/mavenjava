084731ab34fceb9faa534fba071a5c26386c38ba
vamshi0129 <151362743+vamshi0129@users.noreply.github.com>
2024-11-27 16:22:05 +0530

    Add files via upload

---
+++ my-maven-java-project/src/main/java/com/example/my_maven_java_project/App.java
@@ -0,0 +1,13 @@
+package com.example.my_maven_java_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
